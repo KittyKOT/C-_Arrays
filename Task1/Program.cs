d100a9cc93c5afdd34284a32e61aaf319bac9234
Spiky_Coat <cat@test.ru>
2023-12-12 21:03:49 +0300

    Random array with condition

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
 
-int[] array = new int[10] {10, 21, 14, 93, 23, 20, 90, 1, 2, 3};
-// Random rand = new Random();
+int[] array = new int[10]; //{10, 21, 14, 93, 23, 20, 90, 1, 2, 3}
+Random rand = new Random();
 
-// for(int i = 0; i < array.Length; i++)
-// {
-//     array[i] = rand.Next(1, 101);
-// }
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(1, 101);
+}
 
 for(int i = 0;i < array.Length; i++)
 {
