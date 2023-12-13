e725e4a89249b915efee8efd3e37ea64d3fec914
Spiky_Coat <cat@test.ru>
2023-12-13 21:11:47 +0300

    Difference between double max and min numbers

---
+++ Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+double[] array = new double[size];
+Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Math.Round(rand.Next(100) + rand.NextDouble(), 2);
+        Console.Write($"{array[i]} ");
+    }
+
+int FindMax(double[] array)
+{
+    int MaxValue = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > array[MaxValue])
+            {
+                MaxValue = i;
+            }
+    }
+    return MaxValue;
+}
+int FindMin(double[] array)
+{
+    int MinValue = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < array[MinValue])
+            {
+                MinValue = i;
+            }
+    }
+    return MinValue;
+}
+
+int iMax = FindMax(array);
+int iMin = FindMin(array);
+
+Console.Write($"\nРазница между максимальным {array[iMax]} и минимальным {array[iMin]} элементами массива: {array[iMax] - array[iMin]}");
